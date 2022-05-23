81ea3c8e042802834a50771fa43b0c56ed3675c4
Mat Kelly <me@matkelly.com>
2022-05-23 18:26:23 -0400

    Add rudimentary priority queue
    Closes #2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import threading
-from queue import Queue
+from queue import PriorityQueue
 from spider import Spider
 from domain import *
 from general import *
@@ -12,7 +12,7 @@ QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
 NUMBER_OF_THREADS = 4
 # below is defining thread queue
-queue = Queue()
+queue = PriorityQueue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
 
 
@@ -27,15 +27,33 @@ def create_workers():
 # Do the next job in the queue
 def work():
     while True:
-        url = queue.get()
-        Spider.crawl_page(threading.current_thread().name, url)
+        (priority, url) = queue.get()
+        Spider.crawl_page(threading.current_thread().name, url, priority)
         queue.task_done()
+
+
+# Define a priority based on the contents of the URL
+# Note that this is initially intentionally naive
+def get_priority(link) -> int:
+    priority = 10
+    high_priority = 1
+    med_priority = 5
+    high_priority_keys = ['faculty', 'people']
+    med_priority_keys = ['dept', 'departments']
+
+    # Check if any terms in the set of keys above is in the URL
+    # > assign a "higher" priority if so for presedence in processing
+    priority = med_priority if any(u in link for u in med_priority_keys) else priority
+    priority = high_priority if any(u in link for u in high_priority_keys) else priority
+
+    return priority
 
 
 # Each queued link is a new job
 def create_jobs():
     for link in file_to_set(QUEUE_FILE):
-        queue.put(link)
+        priority = get_priority(link)
+        queue.put((priority, link))
     queue.join()
     crawl()
 
@@ -44,7 +62,7 @@ def create_jobs():
 def crawl():
     queued_links = file_to_set(QUEUE_FILE)
     if len(queued_links) > 0:
-        print(str(len(queued_links)) + ' links in the queue')
+        print(f'{str(len(queued_links))} links in the queue')
         create_jobs()
 
 
